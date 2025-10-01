9847bf104cd3eb8f746b8121c3188b75ce8cc6e1
Vincent Dao <vincentdao288@gmail.com>
2025-10-01 15:11:45 +1000

    Added all required active states/ focus states

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ let nav = document.querySelector("nav");
 let contentWrapper = document.querySelector(".body__contentWrapper");
 let allNavLi = document.querySelectorAll("li");
 let lastNavLi = allNavLi[allNavLi.length - 1];
-let heroImg = document.querySelector(".main__heroImg");
+
+let main = document.querySelector("main");
+let heroImg = document.querySelector(".main__heroImg")
+let mobileHeroImgs = ["./images/mobile-image-hero-1.jpg", "./images/mobile-image-hero-2.jpg", "./images/mobile-image-hero-3.jpg"];
+let desktopHeroImgs = ["./images/desktop-image-hero-1.jpg", "./images/desktop-image-hero-2.jpg", "./images/desktop-image-hero-3.jpg"];
+let currentScreenType = "";
+
 let heroTextContainer = document.querySelector(".main__heroTextSection");
 let heroImgContainer = document.querySelector(".main__heroImgSection");
 let imgSliders = document.querySelectorAll(".main__imgSlider");
@@ -14,11 +20,48 @@ let imgSliders = document.querySelectorAll(".main__imgSlider");
 function handleImageType(){
   if(window.innerWidth <= 425){
     heroImg.setAttribute("src", "./images/mobile-image-hero-1.jpg");
+    currentScreenType = "mobile";
   }
   else{
     heroImg.setAttribute("src", "./images/desktop-image-hero-1.jpg");
+    currentScreenType = "desktop";
   }
 }
+
+// Handles image slider functionality
+function handleImageChange(event){
+  let heroImgSrc = heroImg.getAttribute("src");
+  let heroImgIndex = parseInt(heroImgSrc.slice(0, -4).charAt(heroImgSrc.length - 5));
+
+  if(event.target.classList[1] === "backArrow" && heroImgIndex != 1){
+    heroImgIndex -= 1;
+    heroImgIndex = heroImgIndex.toString();
+    heroImg.setAttribute("src", `./images/${currentScreenType}-image-hero-${heroImgIndex}.jpg`);
+  }
+  else if(event.target.classList[1] === "backArrow" && heroImgIndex == 1){
+    heroImgIndex = 3;
+    heroImgIndex = heroImgIndex.toString();
+    heroImg.setAttribute("src", `./images/${currentScreenType}-image-hero-${heroImgIndex}.jpg`);
+  }
+  
+  if(event.target.classList[1] === "forwardArrow" && heroImgIndex != 3){
+    heroImgIndex += 1;
+    heroImgIndex = heroImgIndex.toString();
+    heroImg.setAttribute("src", `./images/${currentScreenType}-image-hero-${heroImgIndex}.jpg`);
+  }
+  else if(event.target.classList[1] === "forwardArrow" && heroImgIndex == 3){
+    heroImgIndex = 1;
+    heroImgIndex = heroImgIndex.toString();
+    heroImg.setAttribute("src", `./images/${currentScreenType}-image-hero-${heroImgIndex}.jpg`);
+  }
+}
+
+// Event-Listeners for image slider (Tested: Success)
+main.addEventListener("click", (e) => {
+  if(e.target.classList[0] === "imgSlider__arrowContainer"){
+    handleImageChange(e);
+  }
+});
 
 // Focus trap for mobileNav
 lastNavLi.addEventListener("keydown", (e) => {
